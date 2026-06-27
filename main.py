e57b4f97b5a3ba7684519896ea892729e9cfafab
Rohit Singh <imrohitsingh4105@gmail.com>
2026-06-27 14:07:08 +0530

    Improve ui with padding and grid

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from tkinter import Tk, filedialog
 from PIL import Image , ImageTk
 from pathlib import Path
 
+
+Win_bg = "#659287"
+Img_label_bg ="#88BDA4"
+
+
 window = Tk()
-window.minsize(400,400)
+window.minsize(500,500)
+window.config(padx=20,pady=20,bg=Win_bg)
 window.title("Mark-IT")
 
 home_dir = Path.home()
@@ -19,24 +25,34 @@ def upload_img():
 
     if file_path:
         img = Image.open(file_path)
-        img.thumbnail((100,100))
+        img.thumbnail((200,200))
         tk_img = ImageTk.PhotoImage(img)
         img_label.config(image=tk_img)
         img_label.image = tk_img
         
         
-title_label = Label(window,text='MARK--IT')
-title_label.grid(row=0,column=0)
+canvas = Canvas(width=200,height=200,highlightthickness=0,bg=Win_bg)
+icon_img = PhotoImage(file='assets/icon-img.png')
+canvas.create_image(100,100,image=icon_img)
+canvas.create_text(100,160,text='Mark-It',font=("Helvetica", 10, "bold"))
+canvas.Image = icon_img
+canvas.grid(row=0,column=1,columnspan=1,padx=20,pady=20)
+        
 
 img_label = Label(window)
-img_label.config(text='Select A image')
-img_label.grid(row=1,column=1)       
+img_label.config(text='Select A image',width=45,height=15,foreground='blue',bg=Img_label_bg)
+img_label.grid(row=1,column=1,padx=20,pady=20)       
         
 
 upload_button = Button(window,text='Upload',command=upload_img)
 upload_button.config(width=20)
-upload_button.grid(row=2,column=1)
+upload_button.grid(row=2,column=0,padx=20,pady=20)
     
+    
+mark_it_button = Button(window,text='Mark-It')
+mark_it_button.config(width=20)
+mark_it_button.grid(row=2,column=2,padx=20,pady=20)
+
 
 
 window.mainloop()
